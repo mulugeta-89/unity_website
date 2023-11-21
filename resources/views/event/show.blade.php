7667032882dbf3f7227c655a7bd5e02ed0036a5a
mulugeta-89 <mulugetahail89@gmail.com>
2023-11-21 14:58:14 +0300

    modifications made on event

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -7,11 +7,39 @@
             {{$event["title"]}}
         </h1>
         <img src="{{asset("/images/public_health.jpg")}}" alt="Image" class="img-fluid mt-4 mb-1 align-items-center">
-        <div class="text-black text-justify">
+        {{-- <div class="text-black">
+            <h5>Starting date:</h5><span>{{$event["start_date"]}}</span>
+            <h5>End Date: {{$event["end_date"]}}</h5>
+            <h5>Location: {{$event["location_name"]}}</h5>
             <p>
                 {{$event["description"]}}
             </p>
-        </div>
+        </div> --}}
+        <div class="text-black">
+            <div class="row">
+              <div class="col-md-6">
+                <h5>Starting Date:</h5>
+                <p>{{$event["start_date"]}}</p>
+              </div>
+              <div class="col-md-6">
+                <h5>End Date:</h5>
+                <p>{{$event["end_date"]}}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <h5>Location:</h5>
+                <p>{{$event["location_name"]}}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <h5>Description:</h5>
+                <p class="text-justify">{{$event["description"]}}</p>
+              </div>
+            </div>
+          </div>
+          
         
   </div>
   </div>
