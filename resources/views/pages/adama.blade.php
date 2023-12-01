5bab36cd6b78f0e7fbf3cbf7e3165ca5ba773705
mulugeta-89 <mulugetahail89@gmail.com>
2023-12-01 13:59:25 +0300

    adama page updated

--- resources/views/pages/adama.blade.php
+++ resources/views/pages/adama.blade.php
@@ -3,27 +3,27 @@
 <div class="container">
     <div class="row">
       <div class="col-md-10 mx-auto">
-        <h1 class="full-width text-center" style="margin-top: 150px;font-size: 46px; text-transform: uppercase; text-align: justify; color:#98b4d4;">
+        <h1 class="full-width text-center" style="margin-top: 150px;font-size: 46px; text-transform: uppercase; text-align: justify; color:#c0c4c9;">
             ADAMA CAMPUS (OROMIYA)
         </h1>
         <img src="{{asset("/images/Adama.jpg")}}" alt="Image" class="img-fluid mt-4 rounded">
         <div class="curriculum-list mt-3">
             <div class="row">
                 <div class="col-md-6 col-sm-12 text-sm-center text-center">
-                    <ul class="header_links_ul">
-                        <h4>Undergraduate Programs</h4>
-                        <li><b>Accounting and Finance</b></li>
-                        <li><b>Business Administration</b></li>
-                        <li><b>Computer Science</b></li>
-                        <li><b>Management</b></li>
-                        <li><b>Marketing Management</b></li>
+                    <ul class="header_links_ul" style="color: white">
+                        <h4 style="color: wheat">Undergraduate Programs</h4>
+                        <li><b><i class="fa-regular fa-hand-point-right"></i> Accounting and Finance</b></li>
+                        <li><b><i class="fa-regular fa-hand-point-right"></i> Business Administration</b></li>
+                        <li><b><i class="fa-regular fa-hand-point-right"></i> Computer Science</b></li>
+                        <li><b><i class="fa-regular fa-hand-point-right"></i> Management</b></li>
+                        <li><b><i class="fa-regular fa-hand-point-right"></i> Marketing Management</b></li>
 
                     </ul>
                 </div>
                 <div class="col-md-6 col-sm-12 text-sm-center text-center">
-                    <ul class="header_links_ul">
-                        <h4>Postgraduate Programs</h4>
-                        <li><b>Master of Business Administration Specialization in Marketing</b></li>
+                    <ul class="header_links_ul" style="color: white">
+                        <h4 style="color: wheat">Postgraduate Programs</h4>
+                        <li><b><i class="fa-regular fa-hand-point-right"></i> Master of Business Administration in Marketing</b></li>
                     </ul>
                 </div>
                 <div class="clearfix"></div>
